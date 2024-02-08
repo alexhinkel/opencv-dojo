88e580de85017bce252d099741e7f3d893c16f9b
Claudio Clemens <asturio@gmx.net>
2024-02-08 11:58:41 +0100

    Unused imports removed

--- src/main/java/de/lv1871/util/ResourceUtils.java
+++ src/main/java/de/lv1871/util/ResourceUtils.java
@@ -1,11 +1,6 @@
 package de.lv1871.util;
 
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public final class ResourceUtils {
 
